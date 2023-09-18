a893a3135334f69ed6647a938cf83d7482b6e99e
TinoMaster <oscarmarcos2673@gmail.com>
2023-09-18 21:54:14 +0200

    ok

--- src/components/app/Nabvar.tsx
+++ src/components/app/Nabvar.tsx
@@ -3,7 +3,7 @@ export const Nabvar = () => {
     <div className="flex justify-center items-center absolute text-white z-10 w-full h-32">
       <div className="flex w-11/12 max-w-720p justify-between items-center">
         {/* Logo */}
-        <div className="text-2xl">EROS</div>
+        <div className="text-2xl">Noah</div>
         {/* Links */}
         <div className="flex gap-3 text-lg">
           <p>Home</p>
--- src/components/menu/ImageMenu.tsx
+++ src/components/menu/ImageMenu.tsx
@@ -5,7 +5,7 @@ import { img_Menu } from "@/utils/images";
 export const ImageMenu = () => {
   return (
     <section className="w-full h-[60vh] relative">
-      <div className="absolute w-full h-full bg-gradient-to-t from-black/40 to-black/60"></div>
+      <div className="absolute w-full h-full bg-gradient-to-t from-white/10 to-black/60"></div>
       <Image
         className="w-full h-full object-cover"
         src={img_Menu}
