4bd57de3852bcd34a8256eb84d789a0d8cc199a1
TinoMaster <oscarmarcos2673@gmail.com>
2023-09-19 20:50:26 +0200

    ok

--- src/components/Banner_Image.tsx
+++ src/components/Banner_Image.tsx
@@ -1,4 +1,7 @@
+"use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { pages } from "@/animations/framers";
 
 interface image {
   src: string;
@@ -16,7 +19,12 @@ export const Banner_Image = ({
   bigTittle: string;
 }) => {
   return (
-    <section className="w-full h-[70vh] relative">
+    <motion.section
+      variants={pages}
+      initial="initial"
+      animate="animate"
+      className="w-full h-[70vh] relative"
+    >
       <div className="absolute flex justify-center items-center w-full h-full bg-gradient-to-t from-black/40 to-black/50">
         <div className="flex flex-col gap-2 text-white w-11/12 md:w-1/3 h-5/6 justify-center">
           <p className="text-slate-300 text-xs md:text-base">{tittle}</p>
@@ -35,6 +43,6 @@ export const Banner_Image = ({
         src={image.src}
         alt="prueba"
       />
-    </section>
+    </motion.section>
   );
 };
